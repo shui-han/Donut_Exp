37f2b8c056a01c6beadcda567a10e43614663550
shui-han <han.shuier@gmail.com>
2020-10-13 18:28:21 +1100

    Update Screen2UnityUnits.cs

--- Donut_example/Assets/Screen2UnityUnits.cs
+++ Donut_example/Assets/Screen2UnityUnits.cs
@@ -5,25 +5,29 @@ using UnityEngine;
 public class Screen2UnityUnits : MonoBehaviour
 {
     public Camera cam; // note: camera is set to orthographic view
-    public Vector2 WorldUnitsInCamera;
+   
     private GameObject DisplayText;
     public GameObject Quad;
-  
+    
     // Start is called before the first frame update
     void Start()
     {
+        Vector2 WorldUnitsInCamera;
         WorldUnitsInCamera.y = cam.GetComponent<Camera>().orthographicSize * 2; // because orthographic  size is measured from the center of the screen
-        WorldUnitsInCamera.x = WorldUnitsInCamera.y *Screen.width / Screen.height;
-
+        float camWidthPix = cam.GetComponent<Camera>().pixelWidth;
+        float camHeightPix = cam.GetComponent<Camera>().pixelHeight;
+        float camWidth = WorldUnitsInCamera.y;
+        WorldUnitsInCamera.x = Mathf.Round(WorldUnitsInCamera.y *(camWidthPix / camHeightPix));
+       
         DisplayText = GameObject.Find("DisplayText");
         DisplayText.gameObject.GetComponent<TMPro.TextMeshPro>().lineSpacing = 10;
        
-        DisplayText.gameObject.GetComponent<MeshRenderer>().enabled = true;
+        
         var videoPlayer = Quad.GetComponent<UnityEngine.Video.VideoPlayer>();
         float videoWidth = videoPlayer.clip.width;
         float videoHeight = videoPlayer.clip.height;
-        DisplayText.gameObject.GetComponent<TMPro.TextMeshPro>().text = "Unity units are (w,h): " + WorldUnitsInCamera.x.ToString() + "," + WorldUnitsInCamera.y.ToString() + "\n\n" + Screen.width.ToString() + "pixels," + Screen.height.ToString() + "pixels," + "\n\n" + "Video Clip in pixels(w, h):\n\n" + videoWidth.ToString() + "," + videoHeight.ToString();
-
+        DisplayText.gameObject.GetComponent<TMPro.TextMeshPro>().text = "Unity units are (w,h): " + WorldUnitsInCamera.x.ToString("#.00") + "," + WorldUnitsInCamera.y.ToString("#.00") + "\n\n" + cam.GetComponent<Camera>().pixelWidth.ToString() + "pixels," + cam.GetComponent<Camera>().pixelHeight.ToString() + "pixels," + "\n\n" + "Video Clip in pixels(w, h):\n\n" + videoWidth.ToString() + "," + videoHeight.ToString();
+        DisplayText.gameObject.GetComponent<MeshRenderer>().enabled = true;
         Quad.SetActive(false);
 
     }
